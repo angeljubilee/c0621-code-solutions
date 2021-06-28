0c9db82cbcefd674ab10fce04f46e0f190f5f6ea
Angel Lee <angeljubilee@gmail.com>
2021-06-28 16:38:43 -0700

    Completed javascript-arrays

---
+++ javascript-arrays/main.js
@@ -0,0 +1,13 @@
+var colors = ['red', 'white', 'blue'];
+console.log('colors[0]: ', colors[0]);
+console.log('colors[0]: ', colors[1]);
+console.log('colors[0]: ', colors[2]);
+console.log('America is ' + colors[0] + ', ' + colors[1] + ', ' + colors[2] + '.');
+colors[2] = 'green';
+console.log('Mexico is ' + colors[0] + ', ' + colors[1] + ', ' + colors[2] + '.');
+
+var students = ['Danny', 'Ryan', 'Tony', 'Lala'];
+var numberOfStudents = students.length;
+console.log('There are ' + numberOfStudents + ' students in the class.');
+console.log('The last student in the array is ' + students[numberOfStudents - 1] + '.');
+console.log('value of students: ', students);
