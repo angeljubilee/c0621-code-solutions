3db9daef864c41dfcf1ab41e529cb3b66877d1dd
Angel Lee <angeljubilee@gmail.com>
2021-06-29 11:26:32 -0700

    Completed javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,116 @@
+// userID used to pull OrderHistory rows
+var userProfile = {
+  userID: 'ASDF13W234235RASDF',
+  userName: 'JS Masher',
+  userEmail: '',
+  userAddress: '',
+  userProfilePage: 'url'
+};
+
+// Use productID to addtoCart (Buy it Again)
+var orderHistory = [
+  {
+    userID: 'ASDF13W234235RASDF',
+    orderNumber: '114-3941689',
+    orderPlaced: '20200804',
+    orderDelivered: '20200804',
+    orderTotal: '34.00',
+    orderDetails: 'url',
+    orderInvoice: 'url',
+    productList: [
+      {
+        productID: '1ASD93R4J5234ASDF',
+        prodoctCategory: 'Book',
+        productName: 'JavaScript ofr impatient programmers',
+        productAuthor: 'Rauschmayer, Dr. Axel',
+        returnWindow: '20200907',
+        productPrice: '31.55',
+        productPage: 'url',
+        productImg: 'url',
+        isReviewed: false,
+        supportAvailable: false
+      }
+    ]
+  },
+  {
+    userID: 'ASDF13W234235RASDF',
+    orderNumer: '113-9984268',
+    orderPlaced: '20200720',
+    orderDelivered: '20200720',
+    orderTotal: '44.53',
+    orderDetails: 'url',
+    orderInvoice: 'url',
+    productList: [
+      {
+        productID: '1ASD93R2823423433ASDF',
+        productCategory: 'Book',
+        productName: 'The Timeless Way of Building',
+        productAuthor: 'Alexander, Christopher',
+        returnWindow: '202008189',
+        productPrice: '17.22',
+        productPage: 'url',
+        productImg: 'url',
+        isReviewed: false,
+        supportAvailable: false
+      }
+    ]
+  },
+  {
+    userID: 'ASDF13W234235RASDF',
+    orderNumer: '114-2875557',
+    orderPlaced: '20200704',
+    orderDelivered: '20200707',
+    orderTotal: '44.53',
+    orderDetails: 'url',
+    orderInvoice: 'url',
+    productList: [
+      {
+        productID: '1ASD93R28234233ASDF',
+        productCategory: 'Electronics',
+        productName: 'Gamecube Controller Adapter',
+        productAuthor: '',
+        returnWindow: '20200805',
+        productPrice: '15.98',
+        productPage: 'url',
+        productImg: 'url',
+        isReviewed: false,
+        supportAvailable: false
+      }
+    ]
+  },
+  {
+    userID: 'ASDF13W234235RASDF',
+    orderNumer: '113-2883177',
+    orderPlaced: '20200703',
+    orderDelivered: '20200705',
+    orderTotal: '138.93',
+    orderDetails: 'url',
+    orderInvoice: 'url',
+    productList: [
+      {
+        productID: '1ASD93234233R28234233ASDF',
+        productCategory: 'Electronics',
+        productName: 'Gamecube Controller Smash Bros',
+        productAuthor: '',
+        returnWindow: '20200804',
+        productPrice: '94.95',
+        productPage: 'url',
+        productImg: 'url',
+        isReviewed: false,
+        supportAvailable: true
+      },
+      {
+        productID: '1ASD93234232334234233ASDF',
+        productCategory: 'Book',
+        productName: 'The Art of Sql',
+        productAuthor: 'Faroult, Stephane',
+        returnWindow: '20200804',
+        productPrice: '33.99',
+        productPage: 'url',
+        productImg: 'url',
+        isReviewed: false,
+        supportAvailable: false
+      }
+    ]
+  }
+];
