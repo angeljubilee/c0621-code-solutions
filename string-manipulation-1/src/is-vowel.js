6482c56e7a8302d4f58ab472ebe333ba8aecee25
Angel Lee <angeljubilee@gmail.com>
2021-07-02 15:27:11 -0700

    Made requested changes

--- string-manipulation-1/src/is-vowel.js
+++ string-manipulation-1/src/is-vowel.js
@@ -3,13 +3,9 @@ function isVowel(char) {
   var capitalChar = char.toUpperCase();
   switch (capitalChar) {
     case ('A') :
-      return true;
     case ('E') :
-      return true;
     case ('I') :
-      return true;
     case ('O') :
-      return true;
     case ('U') :
       return true;
     default:
