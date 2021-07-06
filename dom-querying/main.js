28f0feb8a3bfd91162c25dc28efe22b347aac971
Angel Lee <angeljubilee@gmail.com>
2021-07-06 10:22:10 -0700

    Completed dom-querying

---
+++ dom-querying/main.js
@@ -0,0 +1,9 @@
+console.log('hello word');
+var h1 = document.querySelector('h1');
+console.log('h1 element: ', h1);
+console.dir(h1);
+var explanation = document.querySelector('#explanation');
+console.log('Id explanation: ', explanation);
+console.dir(explanation);
+console.log(document.querySelectorAll('p'));
+console.log(document.querySelectorAll('.example-link'));
