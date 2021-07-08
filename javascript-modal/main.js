148bd3a513ad122a3bf4d08663cd62dd56f85442
Angel Lee <angeljubilee@gmail.com>
2021-07-07 17:47:04 -0700

    Completed requested changes

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -3,17 +3,15 @@ var $container = document.querySelector('.container');
 var $modal = document.querySelector('.modal');
 var $modalButton = document.querySelector('.closeModal');
 
-$modal.hidden = true;
-
 $openButton.addEventListener('click', handleOpenClick);
 $modalButton.addEventListener('click', handleCloseClick);
 
 function handleOpenClick(event) {
   $container.className += ' gray-background';
-  $modal.hidden = false;
+  $modal.className = 'modal';
 }
 
 function handleCloseClick(event) {
   $container.className = 'container flex-center';
-  $modal.hidden = true;
+  $modal.className = 'modal hidden';
 }
