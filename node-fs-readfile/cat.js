230a62905571d473ff9dd1e5d994b089480183d2
Angel Lee <angeljubilee@gmail.com>
2021-08-11 22:46:16 +0000

    Updated cat.js to use forEach

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -2,15 +2,14 @@ const fs = require('fs');
 
 const files = process.argv.slice(2);
 
-async function cat(filenames) {
+function cat(filenames) {
   let result = '';
-  for (var i = 0; i < filenames.length; i++) {
-    const file = filenames[i];
+  filenames.forEach(async file => {
     await fs.readFile(file, 'utf8', (err, data) => {
       if (err) throw err;
       result += data;
     });
-  }
+  });
   console.log(result);
 }
 
