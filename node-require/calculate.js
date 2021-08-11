faa9a6976637ec5e76d3e8eb07c3480f0b2f5407
Angel Lee <angeljubilee@gmail.com>
2021-08-11 17:28:32 +0000

    Completed node-require

---
+++ node-require/calculate.js
@@ -0,0 +1,22 @@
+const add = require('./add');
+const subtract = require('./subtract');
+const multiply = require('./multiply');
+const divide = require('./divide');
+
+let f;
+switch (process.argv[3]) {
+  case 'plus':
+    f = add;
+    break;
+  case 'minus':
+    f = subtract;
+    break;
+  case 'times':
+    f = multiply;
+    break;
+  case 'over':
+    f = divide;
+}
+
+console.log('result: ', f(parseInt(process.argv[2]),
+  parseInt(process.argv[4])));
