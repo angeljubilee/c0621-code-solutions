26bd65f9c212d561d7777e2a2a05041de7e33864
Angel Lee <angeljubilee@gmail.com>
2021-08-18 17:52:35 +0000

    Added amount

--- sql-join/big-spenders.sql
+++ sql-join/big-spenders.sql
@@ -1,4 +1,5 @@
-select "customers"."firstName", "customers"."lastName" from "customers"
+select "customers"."firstName", "customers"."lastName", "payments".amount
+  from "customers"
   join "payments" using ("customerId")
   order by "payments".amount desc
   limit 10;
