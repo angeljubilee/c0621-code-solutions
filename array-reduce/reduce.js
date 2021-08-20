dd83aa3390a9954b9c5e41723fe9a34638423f85
Angel Lee <angeljubilee@gmail.com>
2021-08-20 22:15:11 +0000

    Completed array-reduce

---
+++ array-reduce/reduce.js
@@ -0,0 +1,9 @@
+function reduce(array, reducer, initialValue) {
+  let result = initialValue;
+  for (var i = 0; i < array.length; i++) {
+    result += reducer(array[i], initialValue);
+  }
+  return result;
+}
+
+console.log(reduce([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], (x, y) => x + y, 0));
