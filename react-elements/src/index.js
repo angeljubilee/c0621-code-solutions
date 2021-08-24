5b93c0a6f606504a17410f929032e7e06538b08c
Angel Lee <angeljubilee@gmail.com>
2021-08-24 00:31:26 +0000

    Added # to root

--- react-elements/src/index.js
+++ react-elements/src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 
 const h1 = React.createElement('h1', null, 'Hello, React!');
 
-ReactDOM.render(h1, document.getElementById('root'));
+ReactDOM.render(h1, document.getElementById('#root'));
