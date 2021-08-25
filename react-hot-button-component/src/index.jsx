8082f8fe1fc2183d55c6b04ad4b8a3513f0bc619
Angel Lee <angeljubilee@gmail.com>
2021-08-25 11:51:36 -0700

    Moved color picking logic to render method

--- react-hot-button-component/src/index.jsx
+++ react-hot-button-component/src/index.jsx
@@ -4,47 +4,38 @@ import ReactDOM from 'react-dom';
 class HotButton extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { numClicks: 0, color: '' };
+    this.state = { numClicks: 0 };
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
     let numClicks = this.state.numClicks;
-    let color;
     numClicks++;
-    switch (numClicks) {
-      case 3:
-        color = 'purple';
-        break;
-      case 6:
-        color = 'light-purple';
-        break;
-      case 9:
-        color = 'red';
-        break;
-      case 12:
-        color = 'orange';
-        break;
-      case 15:
-        color = 'yellow';
-        break;
-      case 18:
-        color = 'white';
-        break;
-      default:
-        color = null;
-    }
-
-    if (color) {
-      this.setState({ color: color });
-    }
-
     this.setState({ numClicks: numClicks });
   }
 
   render() {
+    let numClicks = this.state.numClicks;
+    let color;
+    numClicks++;
+    if (numClicks >= 3 && numClicks < 6) {
+      color = 'purple';
+    } else if (numClicks >= 6 && numClicks < 9) {
+      color = 'light-purple';
+    } else if (numClicks >= 9 && numClicks < 12) {
+      color = 'red';
+    } else if (numClicks >= 12 && numClicks < 15) {
+      color = 'orange';
+    } else if (numClicks >= 15 && numClicks < 18) {
+      color = 'yellow';
+    } else if (numClicks >= 18) {
+      color = 'white';
+    } else {
+      color = '';
+    }
+
     return <button onClick={this.handleClick}
-      className={this.state.color}>Hot button</button>;
+      className={color}>Hot button</button>;
   }
 }
 
