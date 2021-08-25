16716aa001c4b28b491ea7960d6ef65b4565afc4
Angel Lee <angeljubilee@gmail.com>
2021-08-25 00:09:09 +0000

    Completed react-events-and-state

---
+++ react-events-and-state/src/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class CustomButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isClicked: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ isClicked: true });
+  }
+
+  render() {
+    let button;
+    if (this.state.isClicked) {
+      return <button>World!</button>;
+    }
+    return <button onClick={this.handleClick}>Hello</button>;
+  }
+}
+
+ReactDOM.render(
+  <CustomButton />,
+  document.getElementById('root')
+);
