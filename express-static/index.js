8b93a2f140f0d44fcf9493295b6feca906f4950f
Angel Lee <angeljubilee@gmail.com>
2021-08-30 17:27:19 +0000

    Completed express-static

---
+++ express-static/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const pubPath = path.join(__dirname, 'public');
+
+console.log(path);
+console.log(pubPath);
+
+const expressStatic = express.static(pubPath);
+app.use(expressStatic);
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
